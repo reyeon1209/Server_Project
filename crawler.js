9b43fe3a314124d8974a9d076df7e6c49ca15f7c
reyeon1209 <reyeon5368@naver.com>
2020-07-06 22:53:48 +0900

    클린코드화

--- crawler.js
+++ crawler.js
@@ -3,118 +3,66 @@ const firebase = require('firebase/app');
 const client = require('cheerio-httpcli');
 var db = admin.database();
 
-function wiki() {
-    var url_2020 = "https://ko.wikipedia.org/wiki/2020%EB%85%84_%EB%8C%80%ED%95%9C%EB%AF%BC%EA%B5%AD%EC%9D%98_%ED%85%94%EB%A0%88%EB%B9%84%EC%A0%84_%EB%93%9C%EB%9D%BC%EB%A7%88_%EB%AA%A9%EB%A1%9D";
-    var url_2019 = "https://ko.wikipedia.org/wiki/2019%EB%85%84_%EB%8C%80%ED%95%9C%EB%AF%BC%EA%B5%AD%EC%9D%98_%ED%85%94%EB%A0%88%EB%B9%84%EC%A0%84_%EB%93%9C%EB%9D%BC%EB%A7%88_%EB%AA%A9%EB%A1%9D";
-    var url_2018 = "https://ko.wikipedia.org/wiki/2018%EB%85%84_%EB%8C%80%ED%95%9C%EB%AF%BC%EA%B5%AD%EC%9D%98_%ED%85%94%EB%A0%88%EB%B9%84%EC%A0%84_%EB%93%9C%EB%9D%BC%EB%A7%88_%EB%AA%A9%EB%A1%9D";
-    var url_2017 = "https://ko.wikipedia.org/wiki/2017%EB%85%84_%EB%8C%80%ED%95%9C%EB%AF%BC%EA%B5%AD%EC%9D%98_%ED%85%94%EB%A0%88%EB%B9%84%EC%A0%84_%EB%93%9C%EB%9D%BC%EB%A7%88_%EB%AA%A9%EB%A1%9D";
-    var url_2016 = "https://ko.wikipedia.org/wiki/2016%EB%85%84_%EB%8C%80%ED%95%9C%EB%AF%BC%EA%B5%AD%EC%9D%98_%ED%85%94%EB%A0%88%EB%B9%84%EC%A0%84_%EB%93%9C%EB%9D%BC%EB%A7%88_%EB%AA%A9%EB%A1%9D";
-    var url_2015 = "https://ko.wikipedia.org/wiki/2015%EB%85%84_%EB%8C%80%ED%95%9C%EB%AF%BC%EA%B5%AD%EC%9D%98_%ED%85%94%EB%A0%88%EB%B9%84%EC%A0%84_%EB%93%9C%EB%9D%BC%EB%A7%88_%EB%AA%A9%EB%A1%9D";
-
+function wiki(crawlYear) {
     var param = {};
-
-    //2020
-    client.fetch(url_2020, param, function(err, $, res){
-        if(err){console.log(err);return;}
-
-        $(".sortable >tbody>tr>td:nth-child(1)").each(function(post) {
-            var title2020 = [];
-            var title_end_2020 = [];
-            var name = $(this).children('a');
-            var color = $(this).parent().attr('style');
-            if(color == "background-color:#E5E5E5"){
-            console.log("end");
-            return false;
-            }
-            if(color != "background-color:#FFFF99"){
-                title_end_2020.push(name.text());
-            }
-            //배열에서 공백은 제외하고 출력
-            if(title_end_2020 != ""){
-                title2020 = title_end_2020;
-                console.log(title2020);
-            }
-
-            // 방영종료 드라마 출력
-            // if(title2019_end != ""){
-            //    console.log(title2019_end);
-            // }
+    let currentYear = new Date().getFullYear();
+    var url = "https://ko.wikipedia.org/wiki/"+ year +
+            "%EB%85%84_%EB%8C%80%ED%95%9C%EB%AF%BC%EA%B5%AD%EC%9D%98_%ED%85%94%EB%A0%88%EB%B9%84%EC%A0%84_%EB%93%9C%EB%9D%BC%EB%A7%88_%EB%AA%A9%EB%A1%9D";
+    var titleList = [];
+    
+    if (crawlYear == currentYear) { // ex) 2020 방영종료
+        client.fetch(url, param, function(err, $, res){
+            if(err){console.log(err);return;}
+    
+            $(".sortable >tbody>tr>td:nth-child(1)").each(function(post) {
+                var temp = [];
+                var name = $(this).children('a');
+                var color = $(this).parent().attr('style');
+                if(color == "background-color:#E5E5E5"){
+                //console.log("end");
+                return false;
+                }
+                if(color != "background-color:#FFFF99"){
+                    temp.push(name.text());
+                }
+                //배열에서 공백은 제외하고 출력
+                if(temp != ""){
+                    titleList = temp;
+                    //console.log(titleList);
+                }
+    
+            });
         });
-    });
 
-
-    //2019
-    client.fetch(url_2019, param, function(err, $, res){
-        if(err){console.log(err);return;}
-
-        $(".sortable >tbody>tr>td:nth-child(1) > a").each(function(post) {
-            var title2019 = [];
-            title2019.push($(this).text());
-                console.log(title2019);
-            });
-        }
-    );
-
-    //2018
-    client.fetch(url_2018, param, function(err, $, res){
-        if(err){console.log(err);return;}
-
-        $(".sortable >tbody>tr>td:nth-child(1) > a").each(function(post) {
-            var title2018 = [];
-            title2018.push($(this).text());
-                console.log(title2018);
-            });
-        }
-    );
-
-    //2017
-    client.fetch(url_2017, param, function(err, $, res){
-        if(err){console.log(err);return;}
-
-        $(".sortable >tbody>tr>td:nth-child(1) > a").each(function(post) {
-            var title2017 = [];
-            title2017.push($(this).text());
-                console.log(title2017);
+        return titleList;
+    }
+    
+    else { // ex) 2015 ~ 2019
+        client.fetch(url, param, function(err, $, res){
+            if(err){console.log(err);return;}
+    
+            $(".sortable >tbody>tr>td:nth-child(1) > a").each(function(post) {
+                titleList.push($(this).text());
+                //console.log(titleList);
                 });
-        }
-    );
-
-    //2016
-    client.fetch(url_2016, param, function(err, $, res){
-        if(err){console.log(err);return;}
-
-        $(".sortable >tbody>tr>td:nth-child(1) > a").each(function(post) {
-            var title2016 = [];
-            title2016.push($(this).text());
-                console.log(title2016);
-            });
-        }
-    );
-
-    //2015
-    client.fetch(url_2015, param, function(err, $, res){
-        if(err){console.log(err);return;}
-
-        $(".sortable >tbody>tr>td:nth-child(1) > a").each(function(post) {
-            var title2015 = [];
-            title2015.push($(this).text());
-                console.log(title2015);
-            });
-        }
-    );
+            }
+        );
+        
+        return titleList;
+    }
 }
 
 function wikiOnAir() {
-    var url_2020 = "https://ko.wikipedia.org/wiki/2020%EB%85%84_%EB%8C%80%ED%95%9C%EB%AF%BC%EA%B5%AD%EC%9D%98_%ED%85%94%EB%A0%88%EB%B9%84%EC%A0%84_%EB%93%9C%EB%9D%BC%EB%A7%88_%EB%AA%A9%EB%A1%9D";
-    var param = {};
+    let currentYear = new Date().getFullYear();
+    var url = "https://ko.wikipedia.org/wiki/"+ currentYear +
+            "%EB%85%84_%EB%8C%80%ED%95%9C%EB%AF%BC%EA%B5%AD%EC%9D%98_%ED%85%94%EB%A0%88%EB%B9%84%EC%A0%84_%EB%93%9C%EB%9D%BC%EB%A7%88_%EB%AA%A9%EB%A1%9D";
+    var titleList = [];
 
-    //2020
-    client.fetch(url_2020, param, function(err, $, res){
+    client.fetch(url, param, function(err, $, res){
         if(err){console.log(err);return;}
 
         $(".sortable >tbody>tr>td:nth-child(1)").each(function(post) {
-            var title2020_ing = [];
-            var title2020_1 = [];
+            var temp = [];
             var name = $(this).children('a');
             var color = $(this).parent().attr('style');
             if(color == "background-color:#E5E5E5"){
@@ -122,28 +70,21 @@ function wikiOnAir() {
                 return false;
             }
             if(color == "background-color:#FFFF99"){
-                title2020_1.push(name.text());
+                temp.push(name.text());
             }
             //배열에서 공백은 제외하고 출력
-            if(title2020_1 != ""){
-                title2020_ing = title2020_1;
-                //console.log(title2020_ing);
-            }
-
-            var onAirdramasRef = db.ref("/onAir");
-            for (var i = 0; i < title2020_ing.length; i++) {
-            onAirdramasRef.push({
-                "name": title2020_ing[i]
-            });
+            if(temp != ""){
+                titleList = temp;
             }
         });
     });
+
+    return titleList;
 }
 
 function daum() { 
     var daumURL = "https://search.daum.net/search?w=tot&DA=UME&t__nil_searchbox=suggest&sug=&sugo=15&sq=" + encodeURIComponent("사이코지만 괜찮아") + "&o=2&q=" + encodeURIComponent("사이코지만 괜찮아");
     var param = {};
-
 
     client.fetch(daumURL, param, function(err, $, res){
         if(err){console.log("error");return;}
@@ -191,7 +132,7 @@ function google() {
     });
 }
 
-module.exports.wiki;
-module.exports.wikiOnAir;
-module.exports.daum;
-module.exports.google;
+module.exports.daumCrawler = daum;
+module.exports.googleCrawler = google;
+module.exports.wikiCrawler = wiki;
+module.exports.wikiOnAirCrawler = wikiOnAir;
