27f8381a66f4964bcd27b6b78f8ecc561f64a676
reyeon1209 <reyeon5368@naver.com>
2020-07-08 03:55:31 +0900

    merge

--- PJ/src/fetchData.js
+++ PJ/src/fetchData.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const data = require('../data/DB.json');
 
 const fetchData = (isOnAir, year) => {
@@ -18,28 +17,3 @@ const fetchData = (isOnAir, year) => {
   return dramaArray;
 };
 module.exports = fetchData;
-=======
-const data = require('../data/DB.json');
-
-const fetchData = (isOnAir, year) => {
-
-    //console.log('fetch isOn' + typeof isOnAir);
-  let key;
-  if (isOnAir === true || isOnAir === 'true') {
-    key = 'onAir';
-  } else {
-    key = year;
-  }
-
-  const dramaObject = data[`${key}`];
-  const dramaArray = Object.values(dramaObject);
-
-  dramaArray.sort(function(a, b){
-    return parseFloat(a.rate) > parseFloat(b.rate) ? -1 : parseFloat(a.rate) < parseFloat(b.rate) ? 1 : 0;
-  });
-  console.log(dramaArray);
-
-  return dramaArray;
-};
-module.exports = fetchData;
->>>>>>> introImg
