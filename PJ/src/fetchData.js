3976be2e807993084bfd2fa27ae19d20967fd66d
hhyein <48901291+hhyein@users.noreply.github.com>
2020-07-08 03:41:37 +0900

    dramaArray sort by rate

--- PJ/src/fetchData.js
+++ PJ/src/fetchData.js
@@ -1,8 +1,6 @@
 const data = require('../data/DB.json');
 
 const fetchData = (isOnAir, year) => {
-
-    console.log('fetch isOn' + typeof isOnAir);
   let key;
   if (isOnAir === true || isOnAir === 'true') {
     key = 'onAir';
@@ -12,7 +10,10 @@ const fetchData = (isOnAir, year) => {
 
   const dramaObject = data[`${key}`];
   const dramaArray = Object.values(dramaObject);
-
+  dramaArray.sort(function(a, b){
+    return parseFloat(a.rate) > parseFloat(b.rate) ? -1 : parseFloat(a.rate) < parseFloat(b.rate) ? 1 : 0;
+  });
+  console.log(dramaArray);
   return dramaArray;
 };
 module.exports = fetchData;
